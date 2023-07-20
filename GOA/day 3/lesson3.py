76bf99e328610c37c7651ea14273c798c27b3ee1
mishochubinidze1 <138606559+mishochubinidze1@users.noreply.github.com>
2023-07-20 17:59:00 +0400

    Add files via upload

---
+++ GOA/day 3/lesson3.py
@@ -0,0 +1,15 @@
+full_name = "misho chubinidze"
+print (full_name[2:9])
+print (full_name[2:9:2])
+#start:finish:step
+
+print (full_name[0:5])
+print (full_name[5:16])
+print (full_name [-3])
+print (full_name.upper())
+
+
+name2 = "    misho    "
+
+print(name2.replace(" ", "#"))
+print(name2.replace("o", "a"))
