399afe8acea515f007b0537699d086ce03c13ffc
mishochubinidze1 <138606559+mishochubinidze1@users.noreply.github.com>
2023-07-20 13:33:11 +0400

    Add files via upload

---
+++ GOA/day1/lesson1.py
@@ -0,0 +1,33 @@
+name = "misho"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "misho" არის ცვლადისთის მნიშვნელობა
+
+surname = "chubinidze"
+ 
+# print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "misho"  #ეს არის str (string) ტიპის ცვლადი
+age = 18 # ეს არის int ( integer ) მთელი რიცხვი
+height = 175.5 # ეს არის float ტიპის ცვლადი ( ათწილადი )
+#boolean (bool) რიპის ცვლადი
+
+knows_programming = True    #True ან False
+is_ugly = False   #snakecase( უნრალოდ წერის სტილი სტანდარტულად )
+
+isUgly = False # ჯავასკრიპტული cameLcase
+
+
+# print( name + " " + surname)
+
+# სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები 
+# print( name + age)
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming))
+
+print(name + " " + str(age) + " " + str(surname) + " " + str(height))
